db95147a78079ddd9f75eaae1000213a0e7f68b5
dm-medvedev <dm.medvedev97@gmail.com>
2020-10-28 22:37:05 +0300

    formatted to pep8

--- hw4/script.py
+++ hw4/script.py
@@ -1,15 +1,35 @@
-from code.session import Session
-from code.columns import String, Integer, Date, Boolean, Float
 from code.base import Base
+from code.columns import Boolean, Date, Float, Integer, String
+from code.session import Session
 
-my_session = Session()      
-my_session.table_from_csv('city', '../hw3/Data/city.csv', {'city': str, 'region': int})
-    
+Data_path = '../hw3/Data/city.csv'
+my_session = Session()
+my_session.table_from_csv('city', Data_path,
+                          {'city': str, 'region': int})
+
+
 class City(Base):
     __session__ = my_session
     city = String()
     region = Integer()
 
 
-city = City(city=42, region='Рим') # error!
-# city = City(region=42, city='Рим') # нет error
+class Movie(Base):
+    __session__ = my_session
+    name = String()
+    dvd = Boolean()
+    dollars = Float()  # millions
+    date = Date()
+
+
+def main():
+    city = City(city=42, region='Рим')  # error!
+    city = City(region=42, city='Рим')  # нет error
+    movie = Movie(name='Fargo', dvd=None, dollars=None, date=None)
+    movie = Movie(name='Fargo', dvd=False, dollars=99.9, date='2011-01-01')
+    city.create()
+    movie.create()
+
+
+if __name__ == '__main__':
+    main()
